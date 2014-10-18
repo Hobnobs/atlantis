2356731ec6edaea54033b1f6dec645b4187bf199
Hobnobs <hedgeknight@gmx.com>
2014-10-18 11:10:37 -0700

    Added THREEx.WindowResize.js to allow window resizing.

---
+++ client/THREEx.WindowResize.js
@@ -0,0 +1,66 @@
+//Copyright (c) 2011 Jerome Etienne, http://jetienne.com
+//
+//Permission is hereby granted, free of charge, to any person obtaining
+//a copy of this software and associated documentation files (the
+//"Software"), to deal in the Software without restriction, including
+//without limitation the rights to use, copy, modify, merge, publish,
+//distribute, sublicense, and/or sell copies of the Software, and to
+//permit persons to whom the Software is furnished to do so, subject to
+//the following conditions:
+//
+//The above copyright notice and this permission notice shall be
+//included in all copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+//LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+//OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+//WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+// This THREEx helper makes it easy to handle window resize.
+// It will update renderer and camera when window is resized.
+//
+// # Usage
+//
+// **Step 1**: Start updating renderer and camera
+//
+// ```var windowResize = THREEx.WindowResize(aRenderer, aCamera)```
+//    
+// **Step 2**: Start updating renderer and camera
+//
+// ```windowResize.stop()```
+// # Code
+
+//
+
+/** @namespace */
+var THREEx	= THREEx 		|| {};
+
+/**
+ * Update renderer and camera when the window is resized
+ * 
+ * @param {Object} renderer the renderer to update
+ * @param {Object} Camera the camera to update
+*/
+THREEx.WindowResize	= function(renderer, camera){
+	var callback	= function(){
+		// notify the renderer of the size change
+		renderer.setSize( window.innerWidth, window.innerHeight );
+		// update the camera
+		camera.aspect	= window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix();
+	}
+	// bind the resize event
+	window.addEventListener('resize', callback, false);
+	// return .stop() the function to stop watching window resize
+	return {
+		/**
+		 * Stop watching window resize
+		*/
+		stop	: function(){
+			window.removeEventListener('resize', callback);
+		}
+	};
+}
